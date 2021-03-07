751d84150c18b6bb540ea1ab2ee570a605f4e06a
Emilio Borraz <emilioborraz@gmail.com>
2021-03-07 21:44:33 +0000

    Setting task owner to current user, using cocoon gem to create nested forms, using active records with 'through' Associations

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,7 +22,7 @@ class TasksController < ApplicationController
   # POST /tasks or /tasks.json
   def create
     @task = Task.new(task_params)
-
+    @task.owner = current_user
     respond_to do |format|
       if @task.save
         format.html { redirect_to @task, notice: "Task was successfully created." }
@@ -64,6 +64,15 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:name, :description, :due_date, :category_id)
+      params.require(:task).permit(:name,
+        :description, 
+        :due_date, 
+        :category_id,
+        participating_users_attributes: [
+          :user_id,
+          :role,
+          :id,
+          :_destroy
+        ])
     end
 end
