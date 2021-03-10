ae39160a164d97953365b18d8d1c390daeb6952c
Emilio Borraz <emilioborraz@gmail.com>
2021-03-10 03:51:41 +0000

    Adding a Model callback to set an internal task code, creating a db seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path
   end
+
   def set_locale
     I18n.locale = 'es'
   end
+
 end
